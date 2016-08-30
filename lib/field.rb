0e2212b25663ee60b6314b3957cbd60e8ffadd87
Chuck Lauer Vose <vosechu@gmail.com>
2016-08-29 17:14:19 -0700

    Starting on fields, afflicted by dog

--- lib/field.rb
+++ lib/field.rb
@@ -1,7 +1,17 @@
 class Field
   attr_reader :acreage
+  attr_accessor :sown_with, :sown_on, :coverage, :maturity
 
   def initialize(acres:)
-    @acreage = acres || 10
+    @acreage  = acres || 10
+    @maturity = 0
+  end
+
+  def sown?
+    !!@sown_with
+  end
+
+  def progress
+
   end
 end
